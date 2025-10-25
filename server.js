644c1a088d7d455a10a74ee1c4fd9f19f8697e89
kamrulhasan2 <kamrulhasan20656@gmail.com>
2025-10-25 15:24:51 +0600

    create a basic base route and install nodemon

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const express = require('express');
 
 const app = express();
 
+app.get('/',(req,res)=>{
+    res.send('Welcome to the express server');
+});
+
+
 app.listen(4000,()=>{
     console.log('I am listening on port no 4000');
 });
