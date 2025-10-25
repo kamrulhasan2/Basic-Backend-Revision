35b239aefdd1ac30b5f10fcc496355d1513fb4ba
kamrulhasan2 <kamrulhasan20656@gmail.com>
2025-10-25 15:30:31 +0600

    create a profile route

--- server.js
+++ server.js
@@ -6,6 +6,17 @@ app.get('/',(req,res)=>{
     res.send('Welcome to the express server');
 });
 
+app.get('/profile',(req,res)=>{
+    const obj = {
+        name: 'kamrul hasan',
+        email: 'kamrul@email.com',
+        phone: '01799999999999',
+        adderss: 'Dhaka, Bangladesh'
+    }
+
+    res.json(obj);
+})
+
 
 app.listen(4000,()=>{
     console.log('I am listening on port no 4000');
