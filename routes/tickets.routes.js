8c06620f1ce321c572db9d254d90607cd6b940fe
kamrulhasan2 <kamrulhasan20656@gmail.com>
2025-10-29 20:21:28 +0600

    create endpoints for show all tickets

--- routes/tickets.routes.js
+++ routes/tickets.routes.js
@@ -2,6 +2,13 @@ const db = require('../db/db');
 const router = require('express').Router();
 
 
+// show all tickets on : http://localhost:4000/app/v1/tickets
+router
+    .route('')
+    .get((_req,res)=>{
+        const tickets = db.find();
+        res.status(200).json(tickets);
+    })
 
 
 module.exports = router;
