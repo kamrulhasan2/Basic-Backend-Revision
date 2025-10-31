69290530cd6fa994e08bf7fcbae2dc555f3f3358
kamrulhasan2 <kamrulhasan20656@gmail.com>
2025-10-31 10:49:44 +0600

    refactor server.js

--- app/app.js
+++ app/app.js
@@ -1,10 +1,10 @@
 require('dotenv').config('../.env');
 const express = require('express');
-const { errorHandler, notFoundHandler } = require('./error');
+const { errorHandler, notFoundHandler } = require('./errors');
 
 const app = express();
 
-app.use(require('./middleware'));
+app.use(require('./middlewire'));
 app.use(require('./routes'));
 
 app.use(notFoundHandler);
